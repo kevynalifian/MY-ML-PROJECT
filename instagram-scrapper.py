07f96aa78e32e2f6f88781f2a9bc45bc8ccd44ed
kevynalifian <89372801+kevynalifian@users.noreply.github.com>
2024-06-05 10:51:54 +0700

    Update and rename nyobaRT.py to instagram-scrapper.py

--- instagram-scrapper.py
+++ instagram-scrapper.py
@@ -1,3 +1,4 @@
+# IMPORT LIBRARY
 import os
 import csv
 import time
@@ -13,6 +14,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import StaleElementReferenceException
 import schedule
 
+# FUNCTION TO OPEN INSTAGRAM USING SELENIUM
 def openselenium():
     driver = Options()
     driver.add_argument("--ignore-certificate-errors")
@@ -22,6 +24,7 @@ def openselenium():
     driver.get("https://www.instagram.com/")
     return driver
 
+# FUNCTION FOR AUTOMATIC LOGIN
 def login(driver, username_str, password_str):
     username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
     password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
@@ -32,6 +35,8 @@ def login(driver, username_str, password_str):
     password.send_keys(password_str)
     log_in = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
     time.sleep(1.5)
+
+# FUNCTION TO FIND THE SEARCH BUTTON
 def search(driver, keyword):
     search_box = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//span[text()='Search']"))).click()
     searchbox = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@placeholder='Search']")))
@@ -40,6 +45,7 @@ def search(driver, keyword):
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, f"//span[contains(text(), '{keyword}')]"))).click()
     time.sleep(2)
 
+# FUNCTION TO GET LINK
 def get_link(driver):
     WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME, "a")))
     anchors = driver.find_elements(By.TAG_NAME, 'a')    
@@ -65,7 +71,8 @@ def get_link(driver):
                 continue 
 
     return hrefs
-    
+
+# FUNCTION TO VISIT LINK AND WRITE TO CSV
 def visit_links(driver, links):
     with open('comments_usernames.csv', 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
@@ -124,6 +131,8 @@ def visit_links(driver, links):
                 # Jika terjadi kesalahan
                 print("Error:", e)
                 print("Could not retrieve data for:", link)
+
+# FUNCTION FOR SAVE TO MONGO
 def save_mongo(filename):
     client = MongoClient('mongodb://localhost:27017/')
     db = client.db_analisis_sentimen
@@ -153,8 +162,8 @@ def save_mongo(filename):
         
 def main():
     driver = openselenium()
-    login(driver, "anokevbcd", "ANOTHERkevyn")
-    search(driver, "pesanunnes")
+    login(driver, "Your_username", "Your_password")
+    search(driver, "Your_keyword")
     links = get_link(driver)
     print(links)
     visit_links(driver, links)
